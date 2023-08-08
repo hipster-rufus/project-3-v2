8b28afabee9f6c6a438fad4e4e82df8b8f3420cb
Kali Lott <kalilott@kalis-mbp.mynetworksettings.com>
2023-08-08 16:11:21 -0600

    Visual changes to pages

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -6,7 +6,17 @@ export default function AboutUs () {
         <div>
             <h1>About Us</h1>
             <div className='form-card'>
-                <h5>Under construction.</h5>
+                <h5><b>BrewFly</b> is a social database app created by brewflies for brewflies.
+                    <br />
+                    <br />
+                    Credits:{' '}
+                        <a href="https://github.com/csherman177" className='text-info'>Courtney</a>
+                        {' '}and{' '}
+                        <a href="https://github.com/hipster-rufus" className='text-info'>Kali</a>
+                    <br />
+                    GitHub Repository:{' '}
+                        <a href="https://github.com/hipster-rufus/project-3-v2" className='text-info'>Project 3</a>
+                </h5>
             </div>  
         </div>
     )
